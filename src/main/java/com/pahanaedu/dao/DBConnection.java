e46360f958550ac1a3b77fad91356b8d324a44a9
Thahirah Razmi <thahirahrazmi@gmail.com>
2025-07-08 09:30:00 +0530

    Add DBConnection.java

---
+++ src/main/java/com/pahanaedu/dao/DBConnection.java
@@ -0,0 +1,50 @@
+package com.pahanaedu.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/online_billing_system_pahana_edu";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "";
+
+    private static DBConnection instance;
+    private Connection connection;
+
+    private DBConnection() {
+        createConnection();
+    }
+
+    public static DBConnection getInstance() {
+        if (instance == null) {
+            synchronized (DBConnection.class) {
+                if (instance == null) {
+                    instance = new DBConnection();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                createConnection(); // Reopen if closed
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    private void createConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
